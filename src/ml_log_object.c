8a5adf83225773f00d705be4b98cf45fd8f323f7
Erik Moqvist <erik.moqvist@gmail.com>
2019-12-07 16:32:18 +0100

    Cleanup.

--- src/ml_log_object.c
+++ src/ml_log_object.c
@@ -121,7 +121,6 @@ void ml_log_object_vprint(struct ml_log_object_t *self_p,
     time_t now;
     struct tm tm;
     size_t length;
-    ssize_t written;
 
     if ((self_p->mask & (1 << level)) == 0) {
         return;
@@ -143,8 +142,7 @@ void ml_log_object_vprint(struct ml_log_object_t *self_p,
     }
 
     buf[length++] = '\n';
-    written = ml_write(module.kmsg_fd, &buf[0], length);
-    (void)written;
+    ml_write(module.kmsg_fd, &buf[0], length);
 }
 
 void ml_log_object_print(struct ml_log_object_t *self_p,
